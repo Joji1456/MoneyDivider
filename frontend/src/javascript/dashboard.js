57ddfd1508c1520fad79b064d5a72e9df40c1ff8
Jojibabu <jojibabutalari.cm.088@gmail.com>
2026-06-26 14:17:02 +0530

    fix all HTML pages structure, add navbar, logout, dashboard stats

--- frontend/src/javascript/dashboard.js
+++ frontend/src/javascript/dashboard.js
@@ -1,23 +1,19 @@
+const user = JSON.parse(localStorage.getItem("user"));
+if (!user) window.location = "index.html";
+
+document.getElementById("userName").innerText = user.name;
+
 fetch(API_BASE + "/groups")
-.then(res => res.json())
-.then(data => {
-    document.getElementById("groups")
-            .innerHTML = data.length;
-});
+    .then(res => res.json())
+    .then(data => document.getElementById("groups").innerText = data.length)
+    .catch(() => {});
 
 fetch(API_BASE + "/expenses")
-.then(res => res.json())
-.then(data => {
-
-    document.getElementById("expenses")
-            .innerHTML = data.length;
-
-    let total = 0;
-
-    data.forEach(e => {
-        total += e.amount;
-    });
-
-    document.getElementById("pending")
-            .innerHTML = "₹" + total;
-});
+    .then(res => res.json())
+    .then(data => {
+        document.getElementById("expenses").innerText = data.length;
+        let total = 0;
+        data.forEach(e => total += e.amount);
+        document.getElementById("pending").innerText = "₹" + total.toFixed(2);
+    })
+    .catch(() => {});
