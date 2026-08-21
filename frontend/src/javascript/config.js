2e76b64851b2a78dddcafec90f04bb3cf31e7e95
Jojibabu <jojibabutalari.cm.088@gmail.com>
2026-08-21 14:17:18 +0530

    Update API URL to moneydivider-2

--- frontend/src/javascript/config.js
+++ frontend/src/javascript/config.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://moneydivider-1.onrender.com";
+const API_BASE = "https://moneydivider-2.onrender.com";
 
 // Keep Render server awake by pinging every 10 minutes
 setInterval(() => fetch(API_BASE + "/users").catch(() => {}), 10 * 60 * 1000);
